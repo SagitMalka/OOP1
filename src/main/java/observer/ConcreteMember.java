69e72a8f8e2fe0ff32f8c05e29a048f7f2f24d1b
SagitMalka <sagitmalka10@gmail.com>
2022-12-30 20:28:20 +0200

    doc

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -1,5 +1,10 @@
 package observer;
 
+/**
+ * ConcreteMember class implements Member Interface. This class describes the client - an object gets notification about changes
+ * The class contains shallow copy of UndoableStringBuilder.
+ * if a registered client decides to unregister - it no longer gets notifications but still able to get the up-to-date usb
+ */
 public class ConcreteMember implements Member {
     private UndoableStringBuilder usb;
 
